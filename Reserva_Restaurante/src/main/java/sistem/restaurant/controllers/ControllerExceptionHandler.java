d4a4c85ebf9870b912bf72af4e385c642ca6080b
Lautaro-Martin-Geredus <lautarogh789@gmail.com>
2024-08-20 11:09:28 -0300

    scaffolding

---
+++ Reserva_Restaurante/src/main/java/sistem/restaurant/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,55 @@
+package sistem.restaurant.controllers;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+import sistem.restaurant.dtos.ErrorApi;
+
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ControllerExceptionHandler
+{
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorApi> handleError(Exception exception)
+    {
+        ErrorApi error = buildError(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorApi> handleError(MethodArgumentNotValidException exception)
+    {
+        ErrorApi error = buildError(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorApi> handleError(ResponseStatusException exception)
+    {
+        ErrorApi error = buildError(exception.getReason(), HttpStatus.valueOf(exception.getStatusCode().value()));
+        return ResponseEntity.status(exception.getStatusCode()).body(error);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorApi> handleError(EntityNotFoundException exception)
+    {
+        ErrorApi error = buildError("Not found - The element required was not found", HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    private ErrorApi buildError(String message, HttpStatus status)
+    {
+        return ErrorApi.builder()
+                .timestamp(String.valueOf(Timestamp.from(ZonedDateTime.now().toInstant())))
+                .error(status.getReasonPhrase())
+                .status(status.value())
+                .message(message)
+                .build();
+    }
+}
---
+++ Reserva_Restaurante/src/main/java/sistem/restaurant/dtos/ErrorApi.java
@@ -0,0 +1,21 @@
+package sistem.restaurant.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorApi
+{
+    private String timestamp;
+
+    private Integer status;
+
+    private String error;
+
+    private String message;
+}
